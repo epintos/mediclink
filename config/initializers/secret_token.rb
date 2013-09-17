e8f138ef65f6f5e8cb273509d864b880b10c71c2
Esteban Pintos <epintos7@gmail.com>
2013-09-17 19:41:30 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mediclink::Application.config.secret_token = '3b1214f25cced8eabda5b9ed92730a5f0e37842e6554edc72a51379bba42cdf6bf3b7dad1aa88084ff66143016c6aaf79302152e5812362bb0d643290d8fb9ab'
