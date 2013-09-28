9e6e2e14f0ac6af5d6b6d688aa13fe500fefaf7c
Esteban Pintos <epintos7@gmail.com>
2013-09-28 18:51:46 -0300

    Added ruby racer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Mediclink::Application.routes.draw do
   devise_for :users
+  devise_scope :users do
+    root :to => "users#show"
+  end
   root :to => "users#show"
   resources :users
   resources :consultations
