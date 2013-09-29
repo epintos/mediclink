f8b69593fbe67a1a63adbe747da7ca1c0b825b07
Matias De Santi <mdesanti90@gmail.com>
2013-09-28 23:32:24 -0300

    Removes some hard coded texts and fixes bug

--- app/models/blood_work.rb
+++ app/models/blood_work.rb
@@ -1,6 +1,6 @@
 class BloodWork < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :colesterol, :user
+  attr_accessible :colesterol, :user, :glucose, :red_blood_cells, :white_blood_cells
 
   scope :for, ->(user) { where(user_id: user.id) }
 end
