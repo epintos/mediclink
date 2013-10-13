4b6cd2ab1ebb618e56937a889dce77db4a7166cd
Esteban Pintos <epintos7@gmail.com>
2013-10-13 19:40:09 -0300

    Added app configuration for secret token. Will travis work?

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Mediclink::Application.config.secret_token = '3b1214f25cced8eabda5b9ed92730a5f0e37842e6554edc72a51379bba42cdf6bf3b7dad1aa88084ff66143016c6aaf79302152e5812362bb0d643290d8fb9ab'
+security = AppConfiguration.for :security
+if security.secret_token.blank?
+  raise "secret_token has not been set. Create a .security.yml file with the secret_token or set the SECURITY_SECRET_KEY_BASE env variable"
+end
+Mediclink::Application.config.secret_token = security.secret_token
